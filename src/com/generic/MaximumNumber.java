9b0028de27174feb3694be00d350995c0ebff0a4
SHAILESH <shaileshdubeyr@gmail.com>
2021-10-15 04:06:33 +0530

    Added : Code for Sorting Arry

--- src/com/generic/MaximumNumber.java
+++ src/com/generic/MaximumNumber.java
@@ -3,39 +3,33 @@ package com.generic;
 import java.util.Scanner;
 
 public class MaximumNumber<T extends Comparable<T>> {
-	T ValueOne, valueTwo, valueThree;
+	T [] multiple;
 
-	public MaximumNumber(T valueOne, T ValueTwo, T valueThree) {
-		this.ValueOne = valueOne;
-		this.valueTwo = ValueTwo;
-		this.valueThree = valueThree;
-	}
+	public MaximumNumber(T [] multiple) {
+			this.multiple = multiple;
+		}
 
 	public T maximum() {
-		return MaximumNumber.maximum(ValueOne, valueTwo, valueThree);
+		return MaximumNumber.maximum(multiple);
 	}
 
-	public static <T extends Comparable<T>> T maximum(T valueOne, T valueTwo, T ValueThree) {
-		T maximum = valueOne;
-		if (valueTwo.compareTo(maximum) > 0)
-			maximum = valueTwo;
-		if (ValueThree.compareTo(maximum) > 0)
-			maximum = ValueThree;
-		return maximum;
-
+	public static <T extends Comparable<T>> T maximum(T []multiple) {
+		final int  ZERO = 0;
+		for (int i = ZERO; i < multiple.length; i++) {
+			for (int j = i; j < multiple.length; j++) {
+				if (multiple[i].compareTo(multiple[j]) < 0) {
+					T temp = multiple[i];
+					multiple[i] = multiple[j];
+					multiple[j] = temp;
+				}
+			}
+		}
+		return multiple[0];
 	}
 
 	public static void main(String[] args) {
-		Scanner userInput = new Scanner(System.in);
-		System.out.println("enter the first one");
-		Integer intOne = userInput.nextInt();
-		System.out.println("enter the second value");
-		Integer intTwo = userInput.nextInt();
-		System.out.println("enter the third value");
-		Integer intThree = userInput.nextInt();
-		new MaximumNumber(intOne, intTwo, intThree).maximum();
-		System.out.println("Print Maximum Number is :" + " " + new MaximumNumber(intOne, intTwo, intThree).maximum());
-
+		Integer [] multiString = {23, 45,35,44,33};
+		int maxValue = new MaximumNumber<Integer>(multiString).maximum();
+		System.out.println("the maximum value of arry "+maxValue);
 	}
-
 }
