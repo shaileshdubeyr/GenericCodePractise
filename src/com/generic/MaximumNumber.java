7727cc743c10475e8aa66022c1287ecb6705f052
SHAILESH <shaileshdubeyr@gmail.com>
2021-10-14 18:13:48 +0530

    Refactor : Float Code

--- src/com/generic/MaximumNumber.java
+++ src/com/generic/MaximumNumber.java
@@ -6,8 +6,7 @@ public class MaximumNumber {
 	
 	static Scanner userInput = new Scanner(System.in);
 	
-	public static Integer findMaximumNumber(Integer valueOne, Integer valueTwo, Integer valueThree) {
-		
+	public static Float findMaximumNumberForFloat(Float valueOne, Float valueTwo, Float valueThree) {
 		if(valueOne.compareTo(valueTwo) > 0 && valueOne.compareTo(valueThree) > 0)
 			return valueOne;
 		else if (valueTwo.compareTo(valueOne) > 0 && valueTwo.compareTo(valueThree) > 0)
@@ -18,12 +17,12 @@ public class MaximumNumber {
 	
 	public static void main(String[] args) {
 		System.out.println("Enter the first number");
-		Integer numberOne = userInput.nextInt();
+		Float numberOne = userInput.nextFloat();
 		System.out.println("Enter the second number");
-		Integer numberTwo = userInput.nextInt();
+		Float numberTwo = userInput.nextFloat();
 		System.out.println("Enter third number");
-		Integer numberThree = userInput.nextInt();
-		Integer maxValue = findMaximumNumber(numberOne, numberTwo, numberThree);
+		Float numberThree = userInput.nextFloat();
+		Float maxValue = findMaximumNumberForFloat(numberOne, numberTwo, numberThree);
 		System.out.println("The maximum value among three is "+maxValue);
 	}
 }
