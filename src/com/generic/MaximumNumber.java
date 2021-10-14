19d1bd1c3b15835099cf08da7823f965e75f2a8a
SHAILESH <shaileshdubeyr@gmail.com>
2021-10-14 16:22:13 +0530

    Added : code for finding maximum value

--- src/com/generic/MaximumNumber.java
+++ src/com/generic/MaximumNumber.java
@@ -2,42 +2,27 @@ package com.generic;
 
 import java.util.Scanner;
 
-public class MaximumNumber<T extends Comparable<T>> {
-	T valueOne;
-	T valueTwo;
-	T valueeThree;
-	public MaximumNumber(T first, T second, T third ) {
-		valueOne = first;
-		valueTwo = second;
-		valueeThree = third;
+public class MaximumNumber {
+	
+	private static final Scanner userInput = new Scanner(System.in);
+	
+	public static void findMaximumNumber() {
+		
+		System.out.println("Enter the fisrst number");
+		int numberOne = userInput.nextInt();
+		System.out.println("Enter the Second number");
+		int numberTwo = userInput.nextInt();
+		System.out.println("Enter the Third number");
+		int numberThree = userInput.nextInt();
+		if(numberOne > numberTwo && numberOne > numberThree)
+			System.out.println("the bigger number is "+numberOne);
+		else if(numberTwo > numberOne && numberTwo > numberThree)
+			System.out.println("the bigger number is "+numberTwo);
+		else
+			System.out.println("the bigger number is "+numberThree);	
 	}
 	
-	public static int userInput() {
-		System.out.println("enter the value");
-		Scanner inputObj = new Scanner(System.in);
-		int integer = inputObj.nextInt();
-		return integer;
-	}
-	
-	public T maximumValue() {
-		if(valueOne.compareTo(valueTwo) > 0) {
-			return valueOne;
-		}
-		else if (valueTwo.compareTo(valueeThree) > 0)
-			return valueTwo;
-		else 
-			return valueeThree;
-	}
 	public static void main(String[] args) {
-		System.out.println("enter the three value ");
-		int firstValue = userInput();
-		int secondValue = userInput();
-		int thirdValue = userInput();
-		
-		MaximumNumber<Integer> numberObj = new MaximumNumber<Integer>(firstValue, secondValue, thirdValue);
-		int maxvalue = numberObj.maximumValue();
-		System.out.println("maximum value is "+maxvalue);
-		
-		
+		findMaximumNumber();
 	}
 }
