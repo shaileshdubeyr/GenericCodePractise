37733e2c64636cd6e29c51094b46fc524f5b0aeb
SHAILESH <shaileshdubeyr@gmail.com>
2021-10-14 07:56:01 +0530

    Added : code Refactor2 for different Input passing instance variable

--- src/com/generic/MaximumNumber.java
+++ src/com/generic/MaximumNumber.java
@@ -1,30 +1,36 @@
 package com.generic;
 
-import java.util.Scanner;
-
 public class MaximumNumber<T extends Comparable<T>> {
 	T valueOne;
 	T valueTwo;
 	T valueeThree;
-	public MaximumNumber(T firstNumber, T secondNumber, T third ) {
+
+	public MaximumNumber(T firstNumber, T secondNumber, T third) {
 		valueOne = firstNumber;
 		valueTwo = secondNumber;
 		valueeThree = third;
 	}
-		
-	public T maximumValue() {
-		if(valueOne.compareTo(valueTwo) > 0) {
-			return valueOne;
-		}
-		else if (valueTwo.compareTo(valueeThree) > 0)
-			return valueTwo;
-		else 
-			return valueeThree;
-	}
 	
-	public static void main(String[] args) {
+	public static <T> void maximumValue(T firstNumber, T secondNumber, T thirdNumber) {
+		System.out.println("first number is " +firstNumber+" second number is " +secondNumber+" third number is " +thirdNumber);
+	}
+
+	public T maximumValue() {
 		
-		MaximumNumber numberObj = new MaximumNumber("1","3","6");
-		System.out.println("the grater value  is "+numberObj.maximumValue());
+		if (valueOne.compareTo(valueTwo) > 0) {
+			return valueOne;
+		} else if (valueTwo.compareTo(valueeThree) > 0)
+			return valueTwo;
+		else
+			return valueeThree;
+		
+	}
+
+	public static void main(String[] args) {
+		MaximumNumber numberObj = new MaximumNumber("1", "3", "6");
+		
+		maximumValue(numberObj.valueOne, numberObj.valueTwo, numberObj.valueeThree);
+		
+		System.out.println("the grater value  is " + numberObj.maximumValue());
 	}
 }
